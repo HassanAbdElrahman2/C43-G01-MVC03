d07f098ffd4e492410a51c674408879f22e154cc
Hassan Abdulrahman <Hassan20352@fci.bu.edu.eg>
2025-03-08 17:37:01 +0200

    Part 02 IKEA Project Structure

---
+++ LinkDev.IKEA.PL/Program.cs
@@ -0,0 +1,47 @@
+namespace LinkDev.IKEA.PL
+{
+    public class Program
+    {
+        // Entry Point For Applecation
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            #region Configure Services
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            #endregion
+
+            var app = builder.Build();
+
+            #region  Configure  HTTP Request Pipeline
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
+
+            #endregion
+
+            app.Run();
+        }
+    }
+}
