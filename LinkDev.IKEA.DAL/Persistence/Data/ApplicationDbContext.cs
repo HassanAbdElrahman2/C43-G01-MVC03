9a33387a1456f76a0e924fd4204867693d0609f5
Hassan Abdulrahman <Hassan20352@fci.bu.edu.eg>
2025-03-08 20:47:23 +0200

    Part 06 Application DbContext

---
+++ LinkDev.IKEA.DAL/Persistence/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using LinkDev.IKEA.DAL.Entities.Departments;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Persistence.Data
+{
+    class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=IKEA;Trusted_Connection=True;");
+
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
