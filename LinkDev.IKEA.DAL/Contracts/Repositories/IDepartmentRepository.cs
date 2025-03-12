393ff07bea95e92e17b94a43a9e7dc8a7612c62c
Hassan Abdulrahman <Hassan20352@fci.bu.edu.eg>
2025-03-12 20:58:02 +0200

    Part 02 Repository DP - Department Repository

---
+++ LinkDev.IKEA.DAL/Contracts/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,18 @@
+﻿using LinkDev.IKEA.DAL.Entities.Departments;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Contracts.Repositories
+{
+  public interface IDepartmentRepository
+    {
+        IEnumerable<Department> GetAll(bool WithTracking=false);
+        Department? GetById(int id);
+        int Add(Department department);
+        int Update(Department department);
+        int Delete(int department);
+    }
+}
---
+++ LinkDev.IKEA.DAL/Persistence/Repositories/DepartmentRepository.cs
@@ -0,0 +1,58 @@
+﻿using LinkDev.IKEA.DAL.Contracts.Repositories;
+using LinkDev.IKEA.DAL.Entities.Departments;
+using LinkDev.IKEA.DAL.Persistence.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkDev.IKEA.DAL.Persistence.Repositories
+{
+    
+    internal class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly ApplicationDbContext _DbContext;
+
+        public DepartmentRepository(ApplicationDbContext dbContext)
+        {
+            _DbContext = dbContext;
+        }
+
+        public Department? GetById(int id)
+        {
+            var Department = _DbContext.Departments.Find(id);
+            return Department;
+        }
+        public IEnumerable<Department> GetAll(bool WithTracking = false)
+        {
+            if (!WithTracking)
+                return _DbContext.Departments.AsNoTracking();
+            return _DbContext.Departments;
+        }
+        public int Add(Department department)
+        {
+            _DbContext.Departments.Add(department);
+
+            return _DbContext.SaveChanges();
+        }
+
+        public int Update(Department department)
+        {
+            _DbContext.Departments.Update(department);
+            return _DbContext.SaveChanges();
+        }
+
+        public int Delete( int id )
+        {
+            var department =_DbContext.Departments.Find(id);
+            if (department is { })
+            {
+                _DbContext.Departments.Remove(department);
+                return _DbContext.SaveChanges();
+            }
+            return 0;
+        }
+    }
+}
