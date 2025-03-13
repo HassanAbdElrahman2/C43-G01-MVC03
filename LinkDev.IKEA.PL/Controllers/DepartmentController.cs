ca70fa1a51d48091b729f7792325075690a737b9
Hassan Abdulrahman <Hassan20352@fci.bu.edu.eg>
2025-03-13 23:19:07 +0200

    Part 07 Department Controller - Details

--- LinkDev.IKEA.PL/Controllers/DepartmentController.cs
+++ LinkDev.IKEA.PL/Controllers/DepartmentController.cs
@@ -1,6 +1,7 @@
 ﻿using LinkDev.IKEA.BLL.Services.Departments;
 using LinkDev.IKEA.PL.ViewModels.Departments;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace LinkDev.IKEA.PL.Controllers
 {
@@ -9,16 +10,48 @@ namespace LinkDev.IKEA.PL.Controllers
         private readonly IDepartmentService _departmentService;
         // Inheritance : DepartmentController is a Controller
         // Composition : Department has a  IdepartmentService
+        #region Services
         public DepartmentController(IDepartmentService departmentService)
         {
             _departmentService = departmentService;
-        }
+        } 
+        #endregion
+
+        #region Index
         [HttpGet]
         public IActionResult Index()
         {
-            var departments= _departmentService.GetDepartments();
+            var departments = _departmentService.GetDepartments();
             var DepartmentViewModel = departments.Select(D => new DepartmentViewModel() { Code = D.Code, Id = D.Id, Name = D.Name, CreationDate = D.CreationDate });
             return View(DepartmentViewModel);
         }
+        #endregion
+
+        #region Details
+        public IActionResult Details (int? id)
+        {
+            if (!id.HasValue)
+                return BadRequest();
+            var department=_departmentService.GetDepartmentById(id.Value);
+
+            if (department is null)
+                return NotFound();
+            var departmentViewModel = new DepartmentDetailsViewModel()
+            {
+                Code = department.Code,
+                Name = department.Name,
+                Description = department.Description??"",
+                CreationDate = department.CreationDate,
+                CreatedBy = department.CreatedBy,
+                CreatedOn = department.CreatedOn,
+                LastModifiedBy = department.LastModifiedBy,
+                LastModifiedOn = department.LastModifiedOn
+
+
+
+            };
+            return View(departmentViewModel);
+        }
+        #endregion
     }
 }
---
+++ LinkDev.IKEA.PL/ViewModels/Departments/DepartmentDetailsViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LinkDev.IKEA.PL.ViewModels.Departments
+{
+    public class DepartmentDetailsViewModel
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Code { get; set; }
+
+        [Display(Name = "Creation Date")]
+        public DateOnly CreationDate { get; set; }
+        [Display(Name = "Created By")]
+        public required string CreatedBy { get; set; }
+        [Display(Name = "Created On")]
+        public DateTime CreatedOn { get; set; }
+        [Display(Name = "Last Modified By")]
+        public required string LastModifiedBy { get; set; }
+        [Display(Name = "Last Modified On")]
+        public DateTime LastModifiedOn { get; set; }
+
+        public required string Description { get; set; }
+    }
+}
