660893c6732a894f4722de423850d23e1b73f5ad
Hassan Abdulrahman <Hassan20352@fci.bu.edu.eg>
2025-03-13 07:36:04 +0200

    Part 05 Department Controller

---
+++ LinkDev.IKEA.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,21 @@
+﻿using LinkDev.IKEA.BLL.Services.Departments;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkDev.IKEA.PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentService _departmentService;
+        // Inheritance : DepartmentController is a Controller
+        // Composition : Department has a  IdepartmentService
+        public DepartmentController(IDepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
