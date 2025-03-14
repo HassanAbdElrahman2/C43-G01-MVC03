2a6d3615d91733b493ad9bc070c15690ca4f2fc8
Hassan Abdulrahman <Hassan20352@fci.bu.edu.eg>
2025-03-14 16:20:35 +0200

    Part 08 Department Controller - Create

--- LinkDev.IKEA.PL/Controllers/DepartmentController.cs
+++ LinkDev.IKEA.PL/Controllers/DepartmentController.cs
@@ -1,4 +1,5 @@
-﻿using LinkDev.IKEA.BLL.Services.Departments;
+﻿using LinkDev.IKEA.BLL.Models.Deoartments;
+using LinkDev.IKEA.BLL.Services.Departments;
 using LinkDev.IKEA.PL.ViewModels.Departments;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
@@ -7,12 +8,14 @@ namespace LinkDev.IKEA.PL.Controllers
 {
     public class DepartmentController : Controller
     {
+        private readonly ILogger<DepartmentController> _logger;
         private readonly IDepartmentService _departmentService;
         // Inheritance : DepartmentController is a Controller
         // Composition : Department has a  IdepartmentService
         #region Services
-        public DepartmentController(IDepartmentService departmentService)
+        public DepartmentController(ILogger<DepartmentController>logger,IDepartmentService departmentService)
         {
+            _logger = logger;
             _departmentService = departmentService;
         } 
         #endregion
@@ -52,6 +55,44 @@ namespace LinkDev.IKEA.PL.Controllers
             };
             return View(departmentViewModel);
         }
+        #endregion
+
+        #region Create
+
+        [HttpGet]
+        public ActionResult Create() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(CreateDepartmentViewModel model)
+        {
+            var massage = "Department Created Successfully";
+            try
+            {
+                if (!ModelState.IsValid) // Server-Side Validation
+                    return View(model);
+                
+                var departmentToCeate = new CreateDepartmentDto(model.Name, model.Description, model.Code, model.CreationDate);
+                var Created = _departmentService.CreateDepartment(departmentToCeate) > 0;
+                if (!Created)
+
+                    massage = "Failed To Create Department";
+            }
+            catch (Exception ex)
+            {
+
+                // 1. Log Exception in Database or External File (By Default Logging System In .Net or By Serialog Package)
+                _logger.LogError(ex.Message, ex.StackTrace!.ToString());
+                // 2. Set Massage
+                massage = "An Error Occurred, Please Try Again Later ";
+            }
+            TempData["Massage"] = massage; 
+
+            return RedirectToAction(nameof(Index));
+            
+        }
+
         #endregion
     }
 }
