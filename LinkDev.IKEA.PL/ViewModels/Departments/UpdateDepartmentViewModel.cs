da2b6c45777c074e9fb1e2f664de955df90860b2
Hassan Abdulrahman <Hassan20352@fci.bu.edu.eg>
2025-04-10 02:52:06 +0200

    Part 01 Partial View

--- LinkDev.IKEA.PL/ViewModels/Departments/UpdateDepartmentViewModel.cs
+++ LinkDev.IKEA.PL/ViewModels/Departments/UpdateDepartmentViewModel.cs
@@ -2,15 +2,8 @@
 
 namespace LinkDev.IKEA.PL.ViewModels.Departments
 {
-    public class UpdateDepartmentViewModel
+    public class UpdateDepartmentViewModel: BaseDepartmentCreateEditViewModel
     {
-
         public int Id { get; set; }
-        public required string Name { get; set; }
-        [Required(ErrorMessage = "Code Is requied ya Hamada")]
-        public required string Code { get; set; }
-        [Display(Name = "Creation Date")]
-        public DateOnly CreationDate { get; set; }
-        public string? Description { get; set; }
     }
 }
--- LinkDev.IKEA.PL/ViewModels/Employees/EmployeeCreateViewModel.cs
+++ LinkDev.IKEA.PL/ViewModels/Employees/EmployeeCreateViewModel.cs
@@ -4,29 +4,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace LinkDev.IKEA.PL.ViewModels.Employees
 {
-    public class EmployeeCreateViewModel
+    public class EmployeeCreateViewModel :BaseEmployeeCreateEditViewModel
     {
-        [MaxLength(50, ErrorMessage = "Max Lenth Should Be 50 Character")]
-        [MinLength(5, ErrorMessage = "Min Lenth Should Be 5 Character")]
-        public required string Name { get; set; }
-        [Range(22, 30)]
-        public int Age { get; set; }
-        [RegularExpression(@"^[1-9]{1,3}-[a-zA-Z]{5,10}-[a-zA-Z]{4,10}-[a-zA-Z]{5,10}"
-        , ErrorMessage = "Address Must Be Like 123-Straet-City-Country")]
-        public string? Address { get; set; }
-        [DataType(DataType.Currency)]
-        public decimal Salary { get; set; }
-        [Display(Name = "Is Active")]
-        public bool IsActive { get; set; }
-        [EmailAddress]
-        public string? Email { get; set; }
-        [Display(Name = "Phone Number")]
-        [Phone]
-        public string? PhoneNumber { get; set; }
-        [Display(Name = "Hiring Date")]
-        public DateTime HiringDate { get; set; }
-        public Gender Gender { get; set; }
-        public EmployeeType EmployeeType { get; set; }
-        public int? DepartmentId { get; set; }
+       
     }
 }
