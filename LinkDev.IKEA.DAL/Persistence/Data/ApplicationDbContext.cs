fb39c6a0544165c76fb3f243a170a6ede8e60dfc
Hassan Abdulrahman <Hassan20352@fci.bu.edu.eg>
2025-04-15 02:25:58 +0200

    Part 06 Install Package and DbContext

--- LinkDev.IKEA.DAL/Persistence/Data/ApplicationDbContext.cs
+++ LinkDev.IKEA.DAL/Persistence/Data/ApplicationDbContext.cs
@@ -1,5 +1,8 @@
 ﻿using LinkDev.IKEA.DAL.Entities.Departments;
 using LinkDev.IKEA.DAL.Entities.Employees;
+using LinkDev.IKEA.DAL.Entities.IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -10,7 +13,7 @@ using System.Threading.Tasks;
 
 namespace LinkDev.IKEA.DAL.Persistence.Data
 {
-   public class ApplicationDbContext : DbContext
+   public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
@@ -20,8 +23,10 @@ namespace LinkDev.IKEA.DAL.Persistence.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
         }
         public DbSet<Department> Departments { get; set; }
         public DbSet<Employee> Employees { get; set; }
+        
     }
 }
